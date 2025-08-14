3e2ec1df09e3b7bf7e790c1a6b4b2cb23f78d343
xshiba <92438125+xshiba@users.noreply.github.com>
2025-08-14 19:20:47 +0700

    Update Loader2.lua

--- Loader2.lua
+++ Loader2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1bdf419d9bd0a0ab17998f930e044886.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/75284c6dce37c94c20e1c9bc5cc4712f.lua"))()
